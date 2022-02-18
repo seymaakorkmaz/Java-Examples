92945c0dd7567db41dcff63fedea92685a7414c3
seyma <seymanurkorkmaz123@gmail.com>
2022-02-18 22:48:50 +0300

    upload

--- example1/src/bmi.java
+++ example1/src/bmi.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class bmi {
 
 	public static void main(String[] args) {
@@ -11,9 +12,21 @@ public class bmi {
 		System.out.print("Enter your height (m) : ");
 		double height = scanner.nextDouble();
 		
-		//BMI
-		
+		// BMI < 18.5  weak
+		// 18.5 <= BMI <= 25 normal
+		// 25 < BMI < 30 overweight
+		// BMI >= 30 obese
 		double bmi = weight / (height * height);
 		System.out.println("BMI = "+ bmi);	
+		
+		if (bmi < 18.5) {
+			System.out.println("weak");
+		}else if((bmi>=18.5) && (bmi <= 25)) {
+			System.out.println("normal");
+		}else if((bmi>25) && (bmi <30 )) {
+			System.out.println("overweight");
+		}else if(bmi >= 30) {
+			System.out.println("obese");
+		}
 	} 
 }
