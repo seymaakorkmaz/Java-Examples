f1e746e7a65f11638a070a3228591966569f6a00
seyma <seymanurkorkmaz123@gmail.com>
2022-02-23 23:50:15 +0300

    mini composition example

---
+++ example5/composition_example/Main.java
@@ -0,0 +1,10 @@
+package composition_example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ example5/composition_example/Tank.java
@@ -0,0 +1,5 @@
+package composition_example;
+
+public class Tank {
+
+}
